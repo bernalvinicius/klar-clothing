ca013f5bca19418e60e26774f38ef97961d14fa5
Vinicius Bernal <viniciusbernal@Viniciuss-MacBook-Air.local>
2020-08-11 07:29:25 +0100

    Header component

--- src/features/KlarClothing.jsx
+++ src/features/KlarClothing.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/header';
 import HomePage from './pages/homepage';
 
 export class KlarClothing extends Component {
@@ -9,6 +10,7 @@ export class KlarClothing extends Component {
   render() {
     return (
       <div>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
         </Switch>
---
+++ src/features/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './styles.scss';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../../assets/images/klar.svg';
+import CartIcon from '../cartIcon';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/contact">
+          CONTACT
+        </Link>
+        <CartIcon />
+      </div>
+    </div>
+  );
+};
+export default Header;
